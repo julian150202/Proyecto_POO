41fe4c9b5d153358d3d0a0ac5d0e04e2bd0bce06
Julian Fadul Neira <jfadulneira@gamil.com>
2023-03-19 20:53:23 -0500

    mapeando ando

---
+++ main.py
@@ -0,0 +1,25 @@
+class Tablero:
+
+    def __init__(self, filas:int, columnas:int, num_minas:int) -> None:
+        
+        self.filas = filas
+        self.columnas = columnas
+        self.num_minas = num_minas
+
+    def crear_tablero(self, filas:int, columnas:int, num_minas:int):
+        pass
+
+    def mostrar_tablero():
+        pass
+
+    def abrir_celda(fila:int,columna:int):
+        pass
+
+    def poner_bandera(fila:int, columna:int):
+        pass
+
+class Jugador:
+
+    def __init__(self) -> None:
+        pass
+
